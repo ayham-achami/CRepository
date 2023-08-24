d53002519bc96499c2817a0978ab65081ceb0ae8
AnastasiaGolts <anastasia.golz@gmail.com>
2023-08-24 13:33:41 +0300

    list manageable

--- Tests/Combine/ListTests.swift
+++ Tests/Combine/ListTests.swift
@@ -10,7 +10,6 @@ import Combine
 import RealmSwift
 import CRepository
 
-// MARK: - CombineTests
 class ListTests: CombineTestCase, ModelsGenerator {
     
     let chat = ChatInfo(id: 1, users: List(["0"]))
@@ -19,114 +18,214 @@ class ListTests: CombineTestCase, ModelsGenerator {
         .init(id: chat.id, users: chat.users)
     }()
     
-    func testStoring() {
-        subscribe("Store manageable chat") { expectation in
-            repository
-                .inMemory
-                .publishManageable
-                .reset() // Given
-                .put(manageableChatInfo(id: 0)) // When
-                .sink("Successful storing of manageable chat", expectation) // Then
-        }
+    override func setUp() async throws {
+        try await reservedRepository
+            .inMemory
+            .manageable
+            .reset()
+            .put(ManageableChatInfo(id: 0, users: .init(["0"])))
     }
     
-    func testInsertingMemberId() {
-        // Given
+    // MARK: - Insertions
+    func testListInsertions() {
         var notificationTick = 0
         reservedRepository
             .inMemory
-            .publishManageable
-            .reset()
-            .put(manageableChatInfo(id: 0))
-            .lazy()
+            .publishLazy
             .fetch(allOf: ManageableChatInfo.self)
             .watchList(at: 0)
             .sink { completion in
                 guard case let .failure(error) = completion else { return }
                 XCTFail("Watch member id insertion failed with error \(error)")
             } receiveValue: { changeset in
-//                switch notificationTick {
-//                case 0:
-//                    XCTAssertEqual(changeset.kind, .initial)
-//                case 1:
-//                    XCTAssertEqual(changeset.kind, .update)
-//                    XCTAssertEqual(changeset.insertions, [1])
-//                case 2:
-//                    XCTAssertEqual(changeset.kind, .update)
-//                    XCTAssertEqual(changeset.insertions, [1])
-//                case 3:
-//                    XCTAssertEqual(changeset.kind, .update)
-//                    XCTAssertEqual(changeset.insertions, [1])
-//                default:
-//                    XCTFail("Receive unknown case")
-//                }
-                print(changeset)
+                switch notificationTick {
+                case 0:
+                    XCTAssertEqual(changeset.kind, .initial)
+                    XCTAssertTrue(changeset.collection.count == 2)
+                case 1:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.insertions, [2])
+                case 2:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.insertions, [3, 4])
+                case 3:
+                    XCTAssertEqual(changeset.kind, .update)
+                    let array = Array(changeset.collection.elements)
+                    XCTAssertEqual(array[changeset.insertions.first!], "new member")
+                case 4:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.insertions, [3])
+                    let array = Array(changeset.collection.elements)
+                    XCTAssertEqual(array[changeset.insertions.first!], "insertion in the middle")
+                case 5:
+                    XCTAssertEqual(changeset.kind, .update)
+                    let elements = Array(changeset.collection)
+                    XCTAssertEqual(elements[1], "second")
+                    XCTAssertEqual(changeset.insertions, [2, 3])
+                default:
+                    XCTFail("Receive unknown case")
+                }
                 notificationTick += 1
-            }.store(in: &subscriptions)
+            }.store(in: &self.subscriptions)
         
-        // When
-        subscribe("Insert member id") { expectation in
+        subscribe("Insert member ids") { expectation in
             reservedRepository
                 .inMemory
                 .publishManageable
                 .modify(ManageableChatInfo.self, with: 0) { manageable in
                     manageable.list.append("1")
                 }
-                .delay(for: .seconds(1), scheduler: RunLoop.current)
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
                 .modify(ManageableChatInfo.self, with: 0) { manageable in
                     manageable.list.append("2")
                 }
-                .delay(for: .seconds(1), scheduler: RunLoop.current)
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
                 .modify(ManageableChatInfo.self, with: 0) { manageable in
-                    manageable.list.append("3")
+                    manageable.list.append(objectsIn: ["3", "4"])
                 }
-                .sink("Successful insertion of member id", expectation)
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    manageable.list.append(objectsIn: ["new member"])
+                }
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    manageable.list.insert("insertion in the middle", at: 3)
+                }
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    manageable.list.insert(contentsOf: ["first", "second"], at: 2)
+                }
+                .sink("Successful inserions of member ids", expectation)
         }
     }
     
-    func testDeletions() {
-        // Given
+    // MARK: - Deletions
+    func testListIDeletions() {
         var notificationTick = 0
-        reservedRepository
-            .inMemory
-            .publishManageable
-            .reset()
-            .put(manageableChat)
-            .sink { completion in
-                guard case let .failure(error) = completion else { return }
-                XCTFail("Watch count fail with error \(error)")
-            } receiveValue: { _ in
-                print("Successful chat adding")
-            }.store(in: &subscriptions)
-        
         reservedRepository
             .inMemory
             .publishLazy
             .fetch(allOf: ManageableChatInfo.self)
-            .watch()
-            .deletions()
+            .watchList(at: 0)
             .sink { completion in
                 guard case let .failure(error) = completion else { return }
-                XCTFail("Watch deletions of member id failed with error \(error)")
+                XCTFail("Watch member id deletion failed with error \(error)")
             } receiveValue: { changeset in
-                // Then
                 switch notificationTick {
                 case 0:
-                    XCTAssertEqual(changeset.count, 1)
-                    XCTAssertEqual(changeset.first, 0)
+                    XCTAssertEqual(changeset.kind, .initial)
+                    XCTAssertTrue(changeset.collection.count == 11)
+                case 1:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.deletions, [10])
+                case 2:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.deletions, [0])
+                case 3:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.deletions, [7, 8])
+                case 4:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.deletions, [5])
+                    XCTAssertEqual(Array(changeset.collection)[5], "7")
+                case 5:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.deletions, [1, 2, 3])
+                    XCTAssertEqual(Array(changeset.collection), ["1", "5", "7"])
                 default:
-                    XCTFail("Receive unknown notification")
+                    XCTFail("Receive unknown case")
                 }
                 notificationTick += 1
-            }.store(in: &subscriptions)
+            }.store(in: &self.subscriptions)
         
-        subscribe("Watch deletions of member id") { expectation in
+        subscribe("Delet member ids") { expectation in
             reservedRepository
                 .inMemory
                 .publishManageable
-                .delay(for: .seconds(1), scheduler: RunLoop.current)
-                .remove(onOf: ManageableChatInfo.self, with: 1)
-                .sink("Successful deletions of member id", expectation)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    (1...10).forEach {
+                        manageable.list.append("\($0)")
+                    }
+                    // ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+                }
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    manageable.list.removeLast()
+                    // ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+                }
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    manageable.list.removeFirst()
+                    // ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+                }
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    manageable.list.removeLast(2)
+                    // ["1", "2", "3", "4", "5", "6", "7"]
+                }
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    manageable.list.remove(at: 5)
+                    // ["1", "2", "3", "4", "5", "7"]
+                }
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    manageable.list.removeSubrange(1...3)
+                    // ["1", "5", "7"]
+                }
+                .sink("Successful deleting of member ids", expectation)
+        }
+    }
+    
+    // MARK: - Modifications
+    func testListModifications() {
+        var notificationTick = 0
+        reservedRepository
+            .inMemory
+            .publishLazy
+            .fetch(allOf: ManageableChatInfo.self)
+            .watchList(at: 0)
+            .sink { completion in
+                guard case let .failure(error) = completion else { return }
+                XCTFail("Watch member id modification failed with error \(error)")
+            } receiveValue: { changeset in
+                switch notificationTick {
+                case 0:
+                    XCTAssertEqual(changeset.kind, .initial)
+                    XCTAssertTrue(changeset.collection.count == 11)
+                case 1:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.modifications, [0])
+                    XCTAssertEqual(changeset.collection.first, "11")
+                case 2:
+                    XCTAssertEqual(changeset.kind, .update)
+                    XCTAssertEqual(changeset.modifications, [1, 2, 3])
+                    XCTAssertEqual(changeset.collection.first, "new value")
+                default:
+                    XCTFail("Receive unknown case")
+                }
+                notificationTick += 1
+            }.store(in: &self.subscriptions)
+        
+        subscribe("Modificate member ids") { expectation in
+            reservedRepository
+                .inMemory
+                .publishManageable
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    (1...10).forEach {
+                        manageable.list.append("\($0)")
+                    }
+                }
+                .delay(for: .milliseconds(100), scheduler: RunLoop.current)
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    manageable.list[0] = "11"
+                }
+                .modify(ManageableChatInfo.self, with: 0) { manageable in
+                    (1...3).forEach {
+                        manageable.list[$0] = "new value"
+                    }
+                }
+                .sink("Successful modificating of member ids", expectation)
         }
     }
 }
