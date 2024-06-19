aa3ff9bac8d6e1c5764c6edc8ddc7a771dc8d88c
Dmitriy Kichigin <d.kichigin@orbita.center>
2024-06-19 14:23:16 +0300

    lock realm version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/realm/realm-cocoa.git", .upToNextMajor(from: "10.38.0"))
+        .package(url: "https://github.com/realm/realm-cocoa.git", exact: "10.44.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
